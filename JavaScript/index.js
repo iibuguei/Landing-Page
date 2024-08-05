579d49b8aa8080cd3fe90fd611127ee77a128a38
Ana Paula <ana@oriontec.com.br>
2024-08-05 09:50:58 -0300

    Ajustes.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,1 @@
-const nome = 'Ana';
-const sobrenome = 'Silva';
-const idade = 19;
-const peso = 57;
-const altura = 1.60;
-let imc = peso / (altura * altura);
-let anoNasc = 2024 - idade;
-
-console.log(`Ana tem ${idade} anos, pesa ${peso} kg`);
-console.log(`tem ${altura} e seu IMC é de ${imc}`);
-console.log(`Ana nasceu em ${anoNasc}`);
+ 
