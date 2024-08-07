a56e238e8cd4a22bd7118faeed41f8dc3e5aad2f
Ana Paula <ana@oriontec.com.br>
2024-08-07 15:23:23 -0300

    Ajustes.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,35 +1,12 @@
-// Minha humilde versão
+const num = Number(prompt('Digite um número'));
+const numTit = document.getElementById('num');
+const texto = document.getElementById('texto');
 
-const nomeA = prompt('Digite seu nome completo:');
-const tamNome = nomeA.length;
-const segLetra = nomeA.charAt(1);
-const indLet = nomeA.indexOf('S');
-const ultIndLet = nomeA.lastIndexOf('n');
-const Ult3Let = nomeA.slice(-3);
-const palav = nomeA.split(' ');
-const Maius = nomeA.toUpperCase();
-const Minus = nomeA.toLowerCase();
-
-document.body.innerHTML += `Seu nome é: ${nome} <br/>`;
-document.body.innerHTML += `Seu nome tem ${tamNome} letras <br/>`;
-document.body.innerHTML += `A segunda letra do seu nome é: ${segLetra} <br/>`;
-document.body.innerHTML += `Qual o primeiro índice da letra S no seu nome? ${indLet} <br/>`;
-document.body.innerHTML += `Qual o último índice da letra N no seu nome? ${ultIndLet} <br/>`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${Ult3Let} <br/>`;
-document.body.innerHTML += `As palavras do seu nome são: ${palav} <br/>`;
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${Maius} <br/>`;"'"
-document.body.innerHTML += `Seu nome com letras minúsculas: ${Minus} <br/>`;
-
-// Versão do Luiz 
-
-const nome = prompt('Digite seu nome completo:');
-
-document.body.innerHTML += `Seu nome é: ${nome} <br/>`;
-document.body.innerHTML += `Seu nome tem ${nome.length} letras <br/>`;
-document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br/>`;
-document.body.innerHTML += `Qual o primeiro índice da letra S no seu nome? ${nome.indexOf('s')} <br/>`;
-document.body.innerHTML += `Qual o último índice da letra N no seu nome? ${nome.lastIndexOf('n')} <br/>`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3, nome.length)} <br/>`;
-document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')} <br/>`;
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase} <br/>`;
-document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase} <br/>`;
+numTit.innerHTML = num;
+numTit.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(num)}</p>`
+texto.innerHTML += `<p>${num} é inteiro: ${Number.isInteger(num)}</p>`
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(num)}</p>`
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(num)}</p>`
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(num)}</p>`
+texto.innerHTML += `<p>com duas casas decimais: ${(num.toFixed(2))}</p>`
