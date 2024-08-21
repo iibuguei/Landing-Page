d3d5c932c6c480acce93987fa1e6d5e3552317ef
Ana Paula <ana@oriontec.com.br>
2024-08-21 16:11:41 -0300

    Ajustes.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,83 @@
-const num = Number(prompt('Digite um número'));
-const numTit = document.getElementById('num');
-const texto = document.getElementById('texto');
+//Exercício IMC
+const form = document.querySelector('#form');
 
-numTit.innerHTML = num;
-numTit.innerHTML = '';
-texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(num)}</p>`
-texto.innerHTML += `<p>${num} é inteiro: ${Number.isInteger(num)}</p>`
-texto.innerHTML += `<p>É NaN: ${Number.isNaN(num)}</p>`
-texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(num)}</p>`
-texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(num)}</p>`
-texto.innerHTML += `<p>com duas casas decimais: ${(num.toFixed(2))}</p>`
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+
+});
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau2', 'Obesidade grau 3'];
+    
+    if (imc >= 39.9) {
+        return nivel[5];
+    }
+
+    if (imc >= 34.9) {
+        return nivel[4];
+    }
+
+    if (imc >= 29.9) {
+        return nivel[3];
+    }
+
+    if (imc >= 24.9) {
+        return nivel[2];
+    }
+
+    if (imc >= 18,5) {
+        return nivel[1];
+    }
+
+    if (imc < 18,5) {
+        return nivel[0];
+    }
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP (className){
+    const p = document.createElement('p');
+    return p;
+}
+    
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resul');
+    resultado.innerHTML = '';
+
+    if(isValid) {
+        p.classList.add('result');
+    } else {
+        p.classList.add('bad');
+    }
+
+    const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}; 
