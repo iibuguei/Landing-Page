f6eb8989acf333c83577c08c304b3cce66fa1c9b
Ana Paula <ana@oriontec.com.br>
2025-01-17 10:49:15 -0300

    Calculadora.

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -1,7 +1,7 @@
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
-        
+
         inicia() {
             this.cliqueBotoes();
             this.pressionaEnter();
@@ -10,56 +10,56 @@ function criaCalculadora() {
         pressionaEnter() {
             this.display.addEventListener('keyup', e => {
                 if (e.keyCode === 13) {
-                    this.RealizaConta();
+                    this.realizaConta();
                 }
-            }); 
+            });
         },
 
-        RealizaConta() {
+        realizaConta() {
             let conta = this.display.value;
-""
+
             try {
-              conta = eval(conta);
+                conta = conta.replace(/x/g, '*');
+                
+                conta = Function(`'use strict'; return (${conta})`)();
 
-              if(!conta) {
-                alert('Conta inválida!')
-                return;
-              }
+                if (isNaN(conta) || conta === Infinity) {
+                    throw new Error('Conta inválida');
+                }
 
-              this.display.value = conta;
-            } catch(e) {
+                this.display.value = conta;
+            } catch (e) {
                 alert('Conta inválida!');
-                return;
+                this.clearDisplay();
             }
         },
 
         clearDisplay() {
-            this.display.value = '';
+            this.display.value = ''; 
         },
 
         apagaUm() {
             this.display.value = this.display.value.slice(0, -1);
         },
 
-
         cliqueBotoes() {
             document.addEventListener('click', e => {
                 const el = e.target;
 
-                if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);   
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
                 }
 
-                if(el.classList.contains('btn-clear')) {
+                if (el.classList.contains('btn-clear')) {
                     this.clearDisplay();
                 }
 
-                if(el.classList.contains('btn-del')) {
-                    this.apagaUm();  
-                }                    
-                
-                if(el.classList.contains('btn-eq')) {
-                    this.RealizaConta();
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             });
         },
